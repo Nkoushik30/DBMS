f09b38c189d40348862710379f1072d6b7b9829b
koushik_nelluri <160224734+Nkoushik30@users.noreply.github.com>
2024-08-24 16:35:22 +0530

    Update Emp_Dept.sql

--- Emp_Dept.sql
+++ Emp_Dept.sql
@@ -1,15 +1,12 @@
 create table dept(deptno number(2), dname  varchar2(20), loc  varchar2(15),  
 constraint pk_dept primary key (deptno));
- 
 create table emp(empno  number(4), ename varchar2(10), job varchar2(9), mgr number(4), hiredate date,
 sal number(7,2), comm number(7,2), deptno number(2), constraint pk_emp primary key (empno),
 constraint fk_deptno foreign key (deptno) references dept (deptno));
-
 insert into dept values(10, 'ACCOUNTING', 'NEW YORK');
 insert into dept values(20, 'RESEARCH', 'DALLAS');
 insert into dept values(30, 'SALES', 'CHICAGO');
 insert into dept values(40, 'OPERATIONS', 'BOSTON');
-
 insert into emp values(7839, 'KING', 'PRESIDENT', null, to_date('17-11-1981','dd-mm-yyyy'), 5000, null, 10);
 insert into emp values(7698, 'BLAKE', 'MANAGER', 7839, to_date('1-5-1981','dd-mm-yyyy'), 2850, null, 30);
 insert into emp values(7782, 'CLARK', 'MANAGER', 7839, to_date('9-6-1981','dd-mm-yyyy'), 2450, null, 10);
@@ -32,3 +29,5 @@ select * from emp order by sal desc;
 select *from emp order by deptno asc,sal desc;
 select* from emp where sal=(select sal from emp where empno=7902);
 select distinct job from emp;
+select ename,empno,sal,sal+sal*0.15 as increased_sal
+from emp
